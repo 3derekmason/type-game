54242552b2de437a16ec6bcf7e7d42ecec467765
Derek Mason <3derekmason@gmail.com>
2023-04-01 16:13:40 -0600

    build: show keystroke count

--- src/components/GameWord.tsx
+++ src/components/GameWord.tsx
@@ -5,8 +5,10 @@ export default function GameWord() {
   const [inputWord, setInputWord] = useState("");
   const target: string = "type game";
   const [match, setMatch] = useState(false);
+  const [typeCount, setTypeCount] = useState(0);
 
   const handleInput = async (e: any) => {
+    setTypeCount(typeCount + 1);
     setInputWord(e.target.value);
   };
 
@@ -17,10 +19,21 @@ export default function GameWord() {
       setMatch(false);
     }
   }, [inputWord]);
+
   return (
     <div>
       <p className={match ? styles.match : styles.noMatch}>{target}</p>
       <input type="text" value={inputWord} onChange={handleInput} />
+      {match ? (
+        <p>
+          You did it! in{" "}
+          {typeCount === target.split("").length
+            ? `a perfect ${typeCount} keystrokes!`
+            : `${typeCount} keystrokes.`}
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
