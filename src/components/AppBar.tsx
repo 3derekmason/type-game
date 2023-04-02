188cb0139ba023507713f3c0baa1864ed04a4e6f
Derek Mason <3derekmason@gmail.com>
2023-04-01 18:53:17 -0600

    build: appbar scaffold and styles

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 import styles from "../styles/AppBar.module.css";
 
 const AppBar = () => {
   return (
     <header className={styles.appBar}>
-      <h1>type game</h1>
-      <nav>Navigation here</nav>
+      <Link href="/">
+        <h1>type game</h1>
+      </Link>
+
+      <nav>
+        <p>About</p>
+        <p>User</p>
+        <p>High Scores</p>
+      </nav>
     </header>
   );
 };
