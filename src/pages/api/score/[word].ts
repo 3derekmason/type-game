59ff420ea5a3878384619a4208abab40979d88b4
Derek Mason <3derekmason@gmail.com>
2023-04-19 18:08:05 -0600

    build: word scores api init

---
+++ src/pages/api/score/[word].ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../../lib/db";
+import { ObjectId } from "mongodb";
+
+async function getWordScores(req: any, res: any) {
+  try {
+    let { db } = await connectToDatabase();
+    const wordScores = await db
+      .collection("scores")
+      .find({ title: new ObjectId(req.body.title) })
+      .toArray();
+    res.status(201).json(wordScores);
+  } catch (error: any) {
+    return res.status(400).json({
+      message: new Error(error).message,
+      success: false,
+    });
+  }
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  switch (req.method) {
+    case "GET": {
+      return getWordScores(req, res);
+    }
+  }
+}
